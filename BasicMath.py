cb7bff9f3edfae40ea3b28b5fef2cbc743e184c8
Varqina <Paramore2510>
2020-10-01 21:41:47 +0200

    FirstOwn Commit 1/10/2020 Warsaw Project

---
+++ BasicMath.py
@@ -0,0 +1,19 @@
+"""
++ add
+- remove
+* multiply
+/ split
+() order
+** condemnation
+// split down
+% modulo
+"""
+def add (a, b):
+    return a + b;
+def remove (a,b):
+    return a - b;
+
+print("add said: "+str(add(5,10)))
+#15
+print("remove said: " + str(remove(10,5)))
+#5
