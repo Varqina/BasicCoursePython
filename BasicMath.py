39d9ababa10e53007585e842279a0a622f96c4cb
Varqina <Paramore2510>
2020-10-01 21:50:49 +0200

    Updated result of Math functions

--- BasicMath.py
+++ BasicMath.py
@@ -13,7 +13,12 @@ def add (a, b):
 def remove (a,b):
     return a - b;
 
-print("add said: "+str(add(5,10)))
-#15
-print("remove said: " + str(remove(10,5)))
-#5
+print("add said: "+str(add(5,10))) #15
+print("remove said: " + str(remove(10,5))) #5
+print(10/2) #5
+print(10/3) #3,33333335
+print (2+2*2) #6
+print ((2+2)*2) #8
+print(2**3)#2*2*2 = 8
+print (10//3) #3
+print(10%3) #1
