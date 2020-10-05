0b98fc6c90abaf13e1790fe63bd6ad348962fd22
Varqina <flisu2@gmail.com>
2020-10-05 23:28:12 +0200

    easy exaples for string. COmbine string and return expected charters from number

---
+++ string.py
@@ -0,0 +1,19 @@
+name = "Piotr"
+surname = "Flis"
+print(name +" "+ surname)
+
+longString = "aaaaaaaaa\
+bbbbbbbbbbb\
+000ccccccccccc\
+kkkkkk"
+
+anotherLongString = """
+a
+1
+2
+"""
+print(name[0]) #return P
+print(name[-1]) # returns last r
+print(name[:])# returns all
+print(name[1:])# return all started from index 1
+print(name[1:3])#return characters 1 and 2
