6711702e4670c4c9e12ac2c89711f74c86b8c6c8
Varqina <flisu2@gmail.com>
2020-10-07 00:14:30 +0200

    if condition

---
+++ conditions.py
@@ -0,0 +1,19 @@
+"""
+>
+<
+==
+!=
+>=
+<=
+"""
+a = 5
+b = 5
+print (5 > 4) # returns true
+if (a > b):
+    print("True")
+elif (a == b):
+    print ("Elif")
+elif (a ==5):
+    print("a = 5")
+else:
+    print("else")
