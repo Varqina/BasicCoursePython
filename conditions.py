2c8a2acf0ef8aec9f9a81f5b5d6e36febe949d4b
Varqina <flisu2@gmail.com>
2020-10-07 00:28:14 +0200

    PEP8 changes

--- conditions.py
+++ conditions.py
@@ -8,12 +8,12 @@
 """
 a = 5
 b = 5
-print (5 > 4) # returns true
-if (a > b):
+print(5 > 4) # returns true
+if a > b:
     print("True")
-elif (a == b):
+elif a == b:
     print ("Elif")
-elif (a ==5):
+elif a ==5:
     print("a = 5")
 else:
     print("else")
