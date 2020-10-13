9a33dbc810f757a7088f896174217a8fcd3ed6fc
Varqina <flisu2@gmail.com>
2020-10-13 22:18:17 +0200

    Tuple

---
+++ tuple.py
@@ -0,0 +1,14 @@
+"""
+tuple - krotka
+
+Imported one and not able to edit it
+Us as often as possible
+Memory saver
+"""
+
+tuple = 1,2,3,4,5,6,"a"
+tupleTheSecond = (1,2,3,4)
+
+print(tuple)
+print(tupleTheSecond)
+print(tuple[2])
