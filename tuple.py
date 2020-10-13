46a071bed8bf501e969438d71b6be47997bce3d1
Varqina <flisu2@gmail.com>
2020-10-13 23:24:11 +0200

    some explanation of tuple and lists

--- tuple.py
+++ tuple.py
@@ -6,8 +6,8 @@ Us as often as possible
 Memory saver
 """
 
-tuple = 1,2,3,4,5,6,"a"
-tupleTheSecond = (1,2,3,4)
+tuple = 1, 2, 3, 4, 5, 6, "a"
+tupleTheSecond = (1, 2, 3, 4)
 
 print(tuple)
 print(tupleTheSecond)
---
+++ typeInType.py
@@ -0,0 +1,23 @@
+name = "Piotr"
+age = 28
+
+name2 = "Radoslaw"
+age2 = 26
+
+person1 = ('Piotr', 28)
+person2 = ("Radoslaw", 26)
+
+personList = [
+    ['Piotr', 29],
+    ['Radoslaw', 26]
+]
+tuplelist = [
+    (2, 4, 4, "Tomek"),
+    (3, 4, 5, "Asia")
+]
+
+print(personList[1][1])
+print(personList[1])
+print(tuplelist[0][3])
+tuplelist.append((2, 2, 2, "Karol"))
+print(tuplelist)
