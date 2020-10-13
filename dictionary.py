f3e6b8e3a02deb17b2a15fc72515d086616e7acf
Varqina <flisu2@gmail.com>
2020-10-13 22:32:39 +0200

    dictionary bases

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+"""
+key - value
+value, key can be mixed type
+
+"""
+#to check all functions
+room = {49:'Piotr', 69:'Radoslaw'}
+rooms = {22:"Radi", 22:"Piotr"}
+roomstoto = {"two":"test", "key":"lock"}
+
+print(room)
+print(rooms)
+print(roomstoto)
+
+rooms[22] = "RAdi"
+print(rooms)
+
+#add element
+
+roomstoto[11]="Element"
+print(roomstoto)
+
+print(rooms.get(22))
