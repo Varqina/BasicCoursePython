2c97e0557564f5d22a959e8700d177dfb6c37f04
Varqina <flisu2@gmail.com>
2020-11-06 00:32:13 +0100

    .copy on object

--- args.py
+++ args.py
@@ -15,3 +15,20 @@ def test_arguments_2(**args):
         print(args[element])
 
 test_arguments_2(amazon="ebay")
+
+
+
+def copy_test(input_list):
+    input_list[0] = 1
+    #return input_list
+
+
+test_list2 = [2,2,2]
+test_list = [2,2,2]
+
+print(copy_test(test_list))
+print(test_list)
+
+print(copy_test(test_list2.copy()))
+print(test_list2)
+
