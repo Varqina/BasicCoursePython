17578bb76ccc07bbe3463cad6e0796eb8025d09b
Varqina <flisu2@gmail.com>
2020-11-22 14:33:34 +0100

    comparison

--- tuple.py
+++ tuple.py
@@ -9,6 +9,14 @@ Memory saver
 tuple = 1, 2, 3, 4, 5, 6, "a"
 tupleTheSecond = (1, 2, 3, 4)
 
+
+test = [1,2,3]
+test2 = (2, 0, test)
+print(test2)
+test2[2].append(2)
+print(type(test2))
+print(test2)
+
 print(tuple)
 print(tupleTheSecond)
 print(tuple[2])
